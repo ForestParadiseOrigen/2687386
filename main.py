7db2d1072b749cf820ed2f9d7a6210f72716a47a
Andres Cordoba <andres.jcv.678@gmail.com>
2023-07-21 11:15:17 -0500

    21_07_23 mostrado datos quemados con listas a html

--- main.py
+++ main.py
@@ -9,3 +9,46 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hola 2687386'
+
+
+def paises():
+    paises = [
+                [
+                    {
+                        "nombre":"America",
+                        "paises":[
+                            "Colombia",
+                            "Peru",
+                            "Canada"
+                            ]
+                    }
+                ],[
+                    {
+                        "nombre":"Europa",
+                        "paises":[
+                            "España",
+                            "Rusia",
+                            "Italia"
+                            ]
+                    }
+                ],[
+                    {
+                        "nombre":"Asia",
+                        "paises":[
+                            "Japon",
+                            "China",
+                            "Armenia"
+                            ]
+                    }
+                ]   
+            ]
+    
+    contador_paises_america = 0
+    for pais in paises[0][0].paises:
+        contador_paises_america += 1
+    print(contador_paises_america)
+    
+    user = 'Andres Cordoba'
+    return render_template ('index.html',
+                            user = user,
+                            continentes = paises)
